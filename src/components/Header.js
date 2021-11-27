50951a9068b6c07e403db09199a6a7ae8cc2775d
HashanSukitha <sukitharox@yahoo.com>
2021-11-27 21:14:27 +1100

    By Hashan

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) =>{
+
+    const onClick = () =>
+    {
+        console.log('Clicked')
+    }
+
+    return(
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Hello' onClick={onClick}/>
+        </header>
+    )
+}
+export default Header
